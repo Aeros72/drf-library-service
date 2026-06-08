973d203004675c6d690fc3b23a70b55d9e69ab20
Aeros72 <pashamelnyk2004@gmail.com>
2026-06-08 16:31:15 +0300

    Implement borrowing list and detail endpoints

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,46 @@
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    class Meta:
+        ordering = ["-borrow_date"]
+
+    def clean(self):
+        if self.expected_return_date <= self.borrow_date:
+            raise ValidationError(
+                "Expected return date must be after borrow date."
+            )
+
+        if (
+            self.actual_return_date
+            and self.actual_return_date < self.borrow_date
+        ):
+            raise ValidationError(
+                "Actual return date cannot be before borrow date."
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user.email} borrowed {self.book.title}"
