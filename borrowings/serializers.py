a56870b643dbfe4113c784d7cfef39a66b3189c8
Aeros72 <pashamelnyk2004@gmail.com>
2026-06-08 16:53:13 +0300

    Implement borrowing creation endpoint

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from rest_framework import serializers
 
 from books.serializers import BookSerializer
@@ -31,3 +33,34 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "expected_return_date",
+        )
+
+    def validate_book(self, value):
+        if value.inventory < 1:
+            raise serializers.ValidationError(
+                "Book inventory must be greater than zero."
+            )
+        return value
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+
+        borrowing = Borrowing.objects.create(
+            user=self.context["request"].user,
+            borrow_date=date.today(),
+            **validated_data
+        )
+
+        book.inventory -= 1
+        book.save()
+
+        return borrowing
